aa732eba84e3420a6dc889b686bc8f6409c3b10b
Alvaro Pinot <alvaro@neatnait.com>
2016-03-07 12:13:53 +0100

    ADD sinon

--- module.test.js
+++ module.test.js
@@ -1,7 +1,13 @@
-QUnit.test('should say hello', function(assert) {
+QUnit.test('should say hello', function (assert) {
   assert.equal(sayHello(), 'hello world');
 });
 
-QUnit.test('should change this test', function(assert) {
+QUnit.test('should change this test', function (assert) {
+  var spyFunction = sinon.spy();
+  spyFunction();
+  assert.ok(spyFunction.called);
+});
+
+QUnit.test('should also change this test', function(assert) {
   assert.ok(false);
 });
